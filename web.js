37055f3d88b6fb998b4539ee074ef0ef3a53793c
Ehsan Ketabchi <eketabch@gmail.com>
2013-07-15 03:50:34 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var indexFile = fs.readFileSync('index.html');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2 !');
+  response.send(indexFile.toString());
 });
 
 var port = process.env.PORT || 5000;
